9a54aba376b0dc532fe10cac7e15aad1e11bb90e
Gleb121region <Gleb121region@users.noreply.github.com>
2023-02-05 16:09:17 +0000

    [MegaLinter] Apply linters fixes

--- src/main/kotlin/parsers/JsonParser.kt
+++ src/main/kotlin/parsers/JsonParser.kt
@@ -27,24 +27,24 @@ class JsonParser {
         var fieldParam = fieldParam
         var searchResult = ""
         var isContinueSearching = true
-        while (isContinueSearching) {   //loop instead of recursion
-            index++ //increment index
+        while (isContinueSearching) { // loop instead of recursion
+            index++ // increment index
             val param = capitalize(fieldParam) // name of country, county, region, city
             val pattern = searchByField(fieldName, param) // Creates a patch to look for
             val cities = JsonPath.parse(json).read<List<String>>(pattern) // list of cities similar in pattern
             println(retrieveValuesByKey(json, fieldName)) // list of countries, counties, regions, cities
             val limitedJsonFile = cities.toString() // json
-            if (limitedJsonFile.matches(Regex(pattern = "^.*?,.*?,.*?,.*?,.*?$"))) {   //check if there are more than 4 elements in the list
-                json = limitedJsonFile //update json string for next iteration of the loop
-                fieldName = fields[index] //update field name for next iteration of the loop
-                fieldParam = scanner.nextLine() //update parameter for next iteration of the loop
-            } else {     //if there are less than 4 elements in the list - stop searching and exit from the loop
-                isContinueSearching = false //set flag too false to exit from the loop
+            if (limitedJsonFile.matches(Regex(pattern = "^.*?,.*?,.*?,.*?,.*?$"))) { // check if there are more than 4 elements in the list
+                json = limitedJsonFile // update json string for next iteration of the loop
+                fieldName = fields[index] // update field name for next iteration of the loop
+                fieldParam = scanner.nextLine() // update parameter for next iteration of the loop
+            } else { // if there are less than 4 elements in the list - stop searching and exit from the loop
+                isContinueSearching = false // set flag too false to exit from the loop
                 searchResult = limitedJsonFile
             }
         }
         if (index == 0) {
-            val cities = JsonPath.parse(searchResult).read<List<String>>("$..city")  // list of cities
+            val cities = JsonPath.parse(searchResult).read<List<String>>("$..city") // list of cities
             if (cities.size > 1) {
                 println(cities.toString()) // print  cities
                 searchResult = searchByField("city", scanner.nextLine())
