c57a9cab8bddf2208a01c9727b692b57e91d8617
LysenkoAleksandra <orange260302@gmail.com>
2023-02-07 20:16:53 +0300

    Raw multiple choice for question area choosing

--- src/main/kotlin/fillingAccountInfo/FillingAccountInfoFlow.kt
+++ src/main/kotlin/fillingAccountInfo/FillingAccountInfoFlow.kt
@@ -8,11 +8,49 @@ import Strings.AccountInfo.WriteProfessionalActivity
 import Strings.Question.ChooseQuestionArea
 import auth.User
 import com.ithersta.tgbotapi.fsm.builders.RoleFilterBuilder
+import com.ithersta.tgbotapi.fsm.entities.triggers.dataButton
+import com.ithersta.tgbotapi.fsm.entities.triggers.onDataCallbackQuery
 import com.ithersta.tgbotapi.fsm.entities.triggers.onEnter
 import com.ithersta.tgbotapi.fsm.entities.triggers.onText
+import dev.inmo.tgbotapi.extensions.api.answers.answer
+import dev.inmo.tgbotapi.extensions.api.edit.reply_markup.editMessageReplyMarkup
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard
 import dev.inmo.tgbotapi.types.UserId
+import dev.inmo.tgbotapi.utils.row
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import states.*
+
+@Serializable
+enum class QuestionArea {
+    Science, Education, Innovations, Entrepreneurship, Finances, Youngsters, Staff, Campus, Society, Strategy, Others
+}
+
+@Serializable
+sealed interface Query
+
+@Serializable
+@SerialName("s")
+class SelectQuery(val area: QuestionArea) : Query
+
+@Serializable
+@SerialName("u")
+class UnselectQuery(val area: QuestionArea) : Query
+
+private var questionAreaToString = mapOf<QuestionArea, String>(
+    QuestionArea.Science to Strings.Question.Science,
+    QuestionArea.Education to Strings.Question.Education,
+    QuestionArea.Innovations to Strings.Question.Innovations,
+    QuestionArea.Entrepreneurship to Strings.Question.Entrepreneurship,
+    QuestionArea.Finances to Strings.Question.Finances,
+    QuestionArea.Youngsters to Strings.Question.Youngsters,
+    QuestionArea.Staff to Strings.Question.Staff,
+    QuestionArea.Campus to Strings.Question.Campus,
+    QuestionArea.Society to Strings.Question.Society,
+    QuestionArea.Strategy to Strings.Question.Strategy,
+    QuestionArea.Others to Strings.Question.Others,
+)
 
 fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.fillingAccountInfoFlow() {
     state<ChooseCityState> {
@@ -20,12 +58,13 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.fillingAc
             sendTextMessage(
                 it,
                 ChooseCity
-                // //TODO: здесь внедрить часть Глеба с выбором города из сложного кнопочного меню
+                ////TODO: здесь внедрить часть Глеба с выбором города из сложного кнопочного меню
             )
         }
         onText {
-            // //TODO: здесь внедрить часть Глеба с выбором города из сложного кнопочного меню
-            val city = it.content.text // /мб если хранить все города листом, то город учстника хранить не словами, а номером в листе?
+            ////TODO: здесь внедрить часть Глеба с выбором города из сложного кнопочного меню
+            val city =
+                it.content.text///мб если хранить все города листом, то город учстника хранить не словами, а номером в листе?
             state.override { WriteProfessionState(city) }
         }
     }
@@ -37,7 +76,7 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.fillingAc
             )
         }
         onText {
-            val profession = it.content.text // мб валидация нужна какая-нибудь?
+            val profession = it.content.text//мб валидация нужна какая-нибудь?
             state.override { WriteOrganizationState(city, profession) }
         }
     }
@@ -49,8 +88,8 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.fillingAc
             )
         }
         onText {
-            val organization = it.content.text // мб валидация нужна какая-нибудь?
-            state.override { ChooseProfessionalAreasState(city, profession, organization) }
+            val organization = it.content.text//мб валидация нужна какая-нибудь?
+            state.override { ChooseProfessionalAreasState(city, profession, organization, emptyList()) }
         }
     }
     state<ChooseProfessionalAreasState> {
@@ -58,11 +97,19 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.fillingAc
             sendTextMessage(
                 it,
                 ChooseProfessionalAreas
-                // //TODO: где-то тут видимо прикол с множественным выбором и видимо обработчиками
-                // /!!!!обработчик "Другое" просит ввода и принимает его
+                ////TODO: где-то тут видимо прикол с множественным выбором и видимо обработчиками
+                ///!!!!обработчик "Другое" просит ввода и принимает его
             )
-            // //после того, как все нужные сферы выбраны, переходим в следующее состояние
-            state.override { WriteProfessionalDescriptionState(city, profession, organization, emptyList()) } // пока пусть эмпти будет
+            ////надо бы и приколы из другого отображать так же
+            ////после того, как все нужные сферы выбраны, переходим в следующее состояние
+            state.override {
+                WriteProfessionalDescriptionState(
+                    city,
+                    profession,
+                    organization,
+                    emptyList()
+                )
+            }//пока пусть эмпти будет
         }
     }
     state<WriteProfessionalDescriptionState> {
@@ -73,19 +120,84 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.fillingAc
             )
         }
         onText {
-            val activity = it.content.text // мб валидация нужна какая-нибудь?
-            state.override { ChooseQuestionAreasState(city, profession, organization, professionalAreas, activity) }
+            val activity = it.content.text//мб валидация нужна какая-нибудь?
+            state.override {
+                ChooseQuestionAreasState(
+                    city,
+                    profession,
+                    organization,
+                    professionalAreas,
+                    activity,
+                    emptyList()
+                )
+            }
         }
     }
     state<ChooseQuestionAreasState> {
         onEnter {
-            sendTextMessage(
-                it,
-                ChooseQuestionArea
-                // /ну тут видимо опять прикол с множественным выбором инлайн кнопок и обработчиками
-            )
+            val keyboard = inlineKeyboard {
+                QuestionArea.values().forEach { area ->
+                    row {
+                        if (area in state.snapshot.questionAreas) {
+                            dataButton("✅${questionAreaToString.get(area)}", UnselectQuery(area))
+                        } else {
+                            dataButton(questionAreaToString.get(area)!!, SelectQuery(area))
+                        }
+                    }
+                }
+            }
+            state.snapshot.messageId?.let { id ->
+                runCatching {
+                    editMessageReplyMarkup(it, id, keyboard)
+                }
+            } ?: run {
+                val message = sendTextMessage(
+                    it,
+                    ChooseQuestionArea,
+                    replyMarkup = keyboard
+                )
+                state.overrideQuietly {
+                    ChooseQuestionAreasState(
+                        city,
+                        profession,
+                        organization,
+                        professionalAreas,
+                        professionalDescription,
+                        questionAreas,
+                        message.messageId
+                    )
+                }
+            }
         }
-        // /создание пользователя в базе данных
-        // /ну я думаю, обработчик будет собирать все номера в лист, а потом создавать в базе им штучки
+        ///создание пользователя в базе данных
+        ///ну я думаю, обработчик будет собирать все номера в лист, а потом создавать в базе им штучки
+        onDataCallbackQuery(SelectQuery::class) { (data, query) ->
+            state.override {
+                ChooseQuestionAreasState(
+                    city,
+                    profession,
+                    organization,
+                    professionalAreas,
+                    professionalDescription,
+                    questionAreas + data.area,
+                    messageId
+                )
+            }
+            answer(query)
+        }
+        onDataCallbackQuery(UnselectQuery::class) { (data, query) ->
+            state.override {
+                ChooseQuestionAreasState(
+                    city,
+                    profession,
+                    organization,
+                    professionalAreas,
+                    professionalDescription,
+                    questionAreas - data.area,
+                    messageId
+                )
+            }
+            answer(query)
+        }
     }
 }
--- src/main/kotlin/states/FillingInfoState.kt
+++ src/main/kotlin/states/FillingInfoState.kt
@@ -1,35 +1,46 @@
 package states
 
+import dev.inmo.tgbotapi.types.MessageId
+import fillingAccountInfo.QuestionArea
 import kotlinx.serialization.Serializable
 
 @Serializable
 object ChooseCityState : DialogState
 
+
 @Serializable
 class WriteProfessionState(
     val city: String
-) : DialogState
+):DialogState
 
 @Serializable
 class WriteOrganizationState(
     val city: String,
     val profession: String
-) : DialogState
+):DialogState
 
 @Serializable
 class ChooseProfessionalAreasState(
     val city: String,
     val profession: String,
-    val organization: String
-) : DialogState
+    val organization: String,
+    val professionalAreas: List<String>
+):DialogState
 
+@Serializable
+class AddProfessionalAreasState(///TODO: понадобится, когда список компетенций дадут и надо будет реализовать "Другое"
+    val city: String,
+    val profession: String,
+    val organization: String,
+    val professionalAreas: List<String>
+):DialogState
 @Serializable
 class WriteProfessionalDescriptionState(
     val city: String,
     val profession: String,
     val organization: String,
     val professionalAreas: List<String>
-) : DialogState
+):DialogState
 
 @Serializable
 class ChooseQuestionAreasState(
@@ -37,5 +48,8 @@ class ChooseQuestionAreasState(
     val profession: String,
     val organization: String,
     val professionalAreas: List<String>,
-    val professionalDescription: String
-) : DialogState
+    val professionalDescription: String,
+    val questionAreas: List<QuestionArea>,
+    val messageId: MessageId? = null
+):DialogState
+
