7d19573d7708c06df32be07e560653a7c60f5381
LysenkoAleksandra <orange260302@gmail.com>
2023-02-09 15:16:40 +0300

    Add multiple choice for professional area choosing

--- src/main/kotlin/states/FillingInfoState.kt
+++ src/main/kotlin/states/FillingInfoState.kt
@@ -5,37 +5,47 @@ import fillingAccountInfo.QuestionArea
 import kotlinx.serialization.Serializable
 
 @Serializable
-object ChooseCityState : DialogState
-
+object WriteNameState: DialogState
+@Serializable
+class ChooseCityState(
+    val name:String
+) : DialogState
 
 @Serializable
 class WriteProfessionState(
+    val name:String,
     val city: String
 ):DialogState
 
 @Serializable
 class WriteOrganizationState(
+    val name:String,
     val city: String,
     val profession: String
 ):DialogState
 
 @Serializable
 class ChooseProfessionalAreasState(
+    val name:String,
     val city: String,
     val profession: String,
     val organization: String,
-    val professionalAreas: List<String>
+    val professionalAreas: List<String>,
+    val messageId:MessageId? = null
 ):DialogState
 
 @Serializable
 class AddProfessionalAreasState(///TODO: понадобится, когда список компетенций дадут и надо будет реализовать "Другое"
+    val name:String,
     val city: String,
     val profession: String,
     val organization: String,
-    val professionalAreas: List<String>
+    val professionalAreas: List<String>,
+    val messageId:MessageId? = null
 ):DialogState
 @Serializable
 class WriteProfessionalDescriptionState(
+    val name:String,
     val city: String,
     val profession: String,
     val organization: String,
@@ -44,6 +54,7 @@ class WriteProfessionalDescriptionState(
 
 @Serializable
 class ChooseQuestionAreasState(
+    val name:String,
     val city: String,
     val profession: String,
     val organization: String,
@@ -53,3 +64,13 @@ class ChooseQuestionAreasState(
     val messageId: MessageId? = null
 ):DialogState
 
+@Serializable
+class AddAccountInfoToDataBaseState(
+    val name:String,
+    val city: String,
+    val profession: String,
+    val organization: String,
+    val professionalAreas: List<String>,
+    val professionalDescription: String,
+    val questionAreas: List<QuestionArea>
+):DialogState
