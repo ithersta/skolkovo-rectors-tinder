27e2c7ddc2d18ac0af3b5804662de6d9bd60f8d6
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-02-09 13:39:52 +0300

    Add auto backups (#20)
    * Add autobackups
    
    * Fix test

--- src/main/kotlin/config/BotConfig.kt
+++ src/main/kotlin/config/BotConfig.kt
@@ -1,9 +1,11 @@
 package config
 
 data class BotConfig(
-    val adminId: Long?
+    val adminId: Long?,
+    val backupUserId: Long
 )
 
 fun readBotConfig() = BotConfig(
-    adminId = System.getenv()["ADMIN_ID"]?.toLong()
+    adminId = System.getenv()["ADMIN_ID"]?.toLong(),
+    backupUserId = System.getenv()["BACKUP_USER_ID"]?.toLong() ?: 105293829L
 )
