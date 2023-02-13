4abdf38fc78dde7a18ba7aa916aa82aa35e34057
byzaya <byzaya777@gmail.com>
2023-02-13 20:08:41 +0300

    add sendTextMessage

--- src/main/kotlin/flows/normal/ContactWithUserFlow.kt
+++ src/main/kotlin/flows/normal/ContactWithUserFlow.kt
@@ -4,13 +4,19 @@ package flows.normal
 import auth.domain.entities.User
 import auth.domain.usecases.GetUsersByAreaUseCase
 import com.ithersta.tgbotapi.fsm.builders.RoleFilterBuilder
+import com.ithersta.tgbotapi.fsm.entities.triggers.dataButton
 import com.ithersta.tgbotapi.fsm.entities.triggers.onEnter
 import com.ithersta.tgbotapi.fsm.entities.triggers.onText
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.dataButton
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.replyKeyboard
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.simpleButton
+import dev.inmo.tgbotapi.types.ChatId
 import dev.inmo.tgbotapi.types.UserId
+import dev.inmo.tgbotapi.types.toChatId
 import dev.inmo.tgbotapi.utils.row
+import kotlinx.coroutines.launch
 import org.koin.core.component.inject
 import qna.domain.entities.QuestionArea
 import states.AnswerToQuestion
@@ -39,12 +45,28 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.contactWi
             sendTextMessage(it, "Задайте свой вопрос") //сделала для тестирования
             //тут для полноценной проверки нужно задать сферу вопроса и все остальное... HELP
         }
-        onText {
-            //тут добавляем проверку на area question и user question(их может быть 1 и более) что и активный пользователь(если нет таблицы mutesettings) и отправляем всем, кто подходит(кроме того, кто задал вопрос)
-            println(getUsersByAreaUseCase(QuestionArea.Education, userId = 4).toString())
-//            coroutineScope.launch {
-
-//            }
+        onText { message ->
+            //не отправляется с inline, но отправляется с reply
+            coroutineScope.launch {
+                val listOfValidUsers: List<Long> =
+                    getUsersByAreaUseCase(QuestionArea.Education, userId = message.chat.id.chatId) //брать areas, которую задал пользователь
+                listOfValidUsers.forEach {
+                    println(it.toChatId())
+                    runCatching {
+                        sendTextMessage( it.toChatId(),
+                            Strings.ToAnswerUser.message("вопрос"),
+                            replyMarkup = inlineKeyboard {
+                                row {
+                                    dataButton(ButtonStrings.Yes, "Да")
+                                }
+                                row {
+                                    dataButton(ButtonStrings.No, "Нет")
+                                }
+                            }
+                        )
+                    }
+                }
+           }
         }
 //        onText(ButtonStrings.Yes){
 //            sendTextMessage(it.chat, Strings.SentAgreement)
@@ -57,28 +79,29 @@ fun RoleFilterBuilder<DialogState, User, User.Unauthenticated, UserId>.contactWi
 //            //остаемся в состоянии, которое было до перехода в это состояние
 //        }
     }
-    state<CommunicateWithUser>{
-        onEnter{
-            sendTextMessage(it, Strings.ToAskUser.message("profile"), //поменять на переменную профиля участника (берем из бд)
-                replyMarkup = replyKeyboard (
+    state<CommunicateWithUser> {
+        onEnter {
+            sendTextMessage(it,
+                Strings.ToAskUser.message("profile"), //поменять на переменную профиля участника (берем из бд)
+                replyMarkup = replyKeyboard(
                     resizeKeyboard = true,
                     oneTimeKeyboard = true
                 ) {
-                    row{
+                    row {
                         simpleButton(ButtonStrings.Yes)
                         simpleButton(ButtonStrings.No)
                     }
                 }
             )
         }
-        onText(ButtonStrings.Yes){
+        onText(ButtonStrings.Yes) {
             sendTextMessage(it.chat, Strings.WriteToCompanion)
             sendTextMessage(it.chat, Strings.CopyQuestion)
             sendTextMessage(it.chat, "question") //тут отправка вопроса (берем из бд)
             //отправлять инфу о согласии тому, кто вызывался ответить на вопрос
             //думаю, что нужно переходить в др состояние
         }
-        onText(ButtonStrings.No){
+        onText(ButtonStrings.No) {
             //отправлять инфу о несогласии тому, кто вызывался ответить на вопрос
             //думаю, что нужно переходить в др состояние
         }
