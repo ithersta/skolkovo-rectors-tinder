e56c24007ad728d18062ae6251e9720d4ca740bf
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-02-14 15:02:41 +0300

    Add SetFeedbackUseCaseTest

---
+++ src/test/kotlin/feedback/domain/usecases/SetFeedbackUseCaseTest.kt
@@ -0,0 +1,32 @@
+package feedback.domain.usecases
+
+import NoOpTransaction
+import feedback.domain.repository.FeedbackRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class SetFeedbackUseCaseTest {
+    private val sampleResponseId = 123L
+    private val sampleFromUserId = 1000L
+
+    @Test
+    fun ok() {
+        val feedbackRepository = mockk<FeedbackRepository>(relaxUnitFun = true)
+        every { feedbackRepository.getAuthorId(sampleResponseId) } returns sampleFromUserId
+        val setFeedback = SetFeedbackUseCase(feedbackRepository, NoOpTransaction)
+        assertEquals(SetFeedbackUseCase.Result.OK, setFeedback(sampleFromUserId, sampleResponseId, true))
+        verify(exactly = 1) { feedbackRepository.setFeedback(sampleResponseId, true) }
+    }
+
+    @Test
+    fun unauthorized() {
+        val feedbackRepository = mockk<FeedbackRepository>(relaxUnitFun = true)
+        every { feedbackRepository.getAuthorId(sampleResponseId) } returns sampleFromUserId
+        val setFeedback = SetFeedbackUseCase(feedbackRepository, NoOpTransaction)
+        assertEquals(SetFeedbackUseCase.Result.Unauthorized, setFeedback(1234L, sampleResponseId, true))
+        verify(exactly = 0) { feedbackRepository.setFeedback(any(), any()) }
+    }
+}
