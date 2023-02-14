47e65390c74a50d0194c24483aa47c0428ad9466
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-02-14 22:25:33 +0300

    Update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.6")
     compileOnly("io.insert-koin:koin-annotations:1.1.1")
     ksp("io.insert-koin:koin-ksp-compiler:1.1.1")
-    ksp("com.ithersta.tgbotapi:boot-ksp:0.1.4")
+    ksp("com.ithersta.tgbotapi:boot-ksp:0.1.5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
     testImplementation("io.insert-koin:koin-test-junit5:3.3.3")
     testImplementation("io.mockk:mockk:1.13.4")
