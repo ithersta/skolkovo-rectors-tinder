25ffcccfba4b514fe3d5a7156b85a28552b4960f
byzaya <byzaya@users.noreply.github.com>
2023-02-16 15:22:29 +0000

    [MegaLinter] Apply linters fixes

--- src/main/kotlin/qna/data/repository/QuestionRepositoryImpl.kt
+++ src/main/kotlin/qna/data/repository/QuestionRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package qna.data.repository
 
 import auth.domain.entities.Question
-import qna.domain.repository.QuestionRepository
 import org.jetbrains.exposed.sql.batchInsert
 import org.jetbrains.exposed.sql.insertAndGetId
 import qna.data.tables.QuestionAreas
 import qna.data.tables.Questions
+import qna.domain.repository.QuestionRepository
 
 class QuestionRepositoryImpl : QuestionRepository {
     override fun add(question: Question.Details) {
@@ -15,8 +15,8 @@ class QuestionRepositoryImpl : QuestionRepository {
             it[subject] = question.subject
             it[text] = question.text
         }
-        QuestionAreas.batchInsert(question.areas){
-            //тут нужно передавать id вопроса, пока не уверена в правильности
+        QuestionAreas.batchInsert(question.areas) {
+            // тут нужно передавать id вопроса, пока не уверена в правильности
             this[QuestionAreas.questionId] = id
             this[QuestionAreas.area] = it
         }
