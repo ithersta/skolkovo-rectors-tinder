10a3a9d583ca2b7f9c8c3ea4c4b64aa2da7c8466
ithersta <ithersta@users.noreply.github.com>
2023-02-22 08:31:53 +0000

    [MegaLinter] Apply linters fixes

--- src/test/kotlin/auth/domain/usecases/RegisterUserUseCaseTest.kt
+++ src/test/kotlin/auth/domain/usecases/RegisterUserUseCaseTest.kt
@@ -60,7 +60,7 @@ internal class RegisterUserUseCaseTest {
         every { userRepository.containsUserWithPhoneNumber(samplePhoneNumber) } returns true
         val phoneNumberIsAllowedUseCase = mockk<PhoneNumberIsAllowedUseCase>()
         every { phoneNumberIsAllowedUseCase.invoke(details.phoneNumber) } returns
-                PhoneNumberIsAllowedUseCase.Result.DuplicatePhoneNumber
+            PhoneNumberIsAllowedUseCase.Result.DuplicatePhoneNumber
         val registerUser = RegisterUserUseCase(phoneNumberIsAllowedUseCase, userRepository, NoOpTransaction)
         assertEquals(RegisterUserUseCase.Result.DuplicatePhoneNumber, registerUser(details))
         verify(exactly = 0) { userRepository.add(any()) }
@@ -74,7 +74,7 @@ internal class RegisterUserUseCaseTest {
         every { userRepository.containsUserWithPhoneNumber(samplePhoneNumber) } returns false
         val phoneNumberIsAllowedUseCase = mockk<PhoneNumberIsAllowedUseCase>()
         every { phoneNumberIsAllowedUseCase.invoke(details.phoneNumber) } returns
-                PhoneNumberIsAllowedUseCase.Result.PhoneNumberNotAllowed
+            PhoneNumberIsAllowedUseCase.Result.PhoneNumberNotAllowed
         val registerUser = RegisterUserUseCase(phoneNumberIsAllowedUseCase, userRepository, NoOpTransaction)
         assertEquals(RegisterUserUseCase.Result.PhoneNumberNotAllowed, registerUser(details))
         verify(exactly = 0) { userRepository.add(any()) }
@@ -89,7 +89,7 @@ internal class RegisterUserUseCaseTest {
         every { userRepository.add(details) } returns Unit
         val phoneNumberIsAllowedUseCase = mockk<PhoneNumberIsAllowedUseCase>()
         every { phoneNumberIsAllowedUseCase.invoke(details.phoneNumber) } returns
-                PhoneNumberIsAllowedUseCase.Result.OK
+            PhoneNumberIsAllowedUseCase.Result.OK
         val registerUser = RegisterUserUseCase(phoneNumberIsAllowedUseCase, userRepository, NoOpTransaction)
         assertEquals(RegisterUserUseCase.Result.OK, registerUser(details))
         verify(exactly = 1) { userRepository.add(any()) }
