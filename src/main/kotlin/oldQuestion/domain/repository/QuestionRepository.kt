f01ce110aa61942debaa87b93b28df74891756b0
Gleb121region <Gleb121region@users.noreply.github.com>
2023-02-25 19:01:09 +0000

    [MegaLinter] Apply linters fixes

--- src/main/kotlin/oldQuestion/domain/repository/QuestionRepository.kt
+++ src/main/kotlin/oldQuestion/domain/repository/QuestionRepository.kt
@@ -1,9 +1,9 @@
 package oldQuestion.domain.repository
 
 interface QuestionRepository {
-    //subject, Q_ID
+    // subject, Q_ID
     fun getSubjectsByUserIdAndIsClosed(userId: Long): Map<Long, String>
 
-    //name, phone
+    // name, phone
     fun getRespondentIdByQuestion(questionId: Long): Map<String, String>
 }
