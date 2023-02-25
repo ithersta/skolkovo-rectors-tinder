cef8c0a3af18110aa6a71571d552e8c2c732b97f
LysenkoAleksandra <orange260302@gmail.com>
2023-02-25 19:58:24 +0300

    Fix import

--- src/main/kotlin/StateMachine.kt
+++ src/main/kotlin/StateMachine.kt
@@ -17,7 +17,7 @@ import feedback.telegram.flows.feedbackFlow
 import menus.adminMenu
 import menus.normalMenu
 import mute.telegram.muteFlow
-import qna.flows.askQuestionFlow
+import qna.telegram.flows.askQuestionFlow
 
 @StateMachine(baseQueryKClass = Query::class)
 val stateMachine = stateMachine<DialogState, User, UserId>(
