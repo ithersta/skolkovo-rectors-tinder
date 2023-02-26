abeebaa64832a8b7bae6dcc9e46b898d8d0a93b0
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-02-26 13:48:29 +0300

    Respect non right away notification preference

--- src/main/kotlin/qna/data/repository/UserAreasRepositoryImpl.kt
+++ src/main/kotlin/qna/data/repository/UserAreasRepositoryImpl.kt
@@ -2,7 +2,10 @@ package qna.data.repository
 
 import auth.data.tables.UserAreas
 import mute.data.tables.MuteSettings
+import notifications.data.tables.NotificationPreferences
+import notifications.domain.entities.NotificationPreference
 import org.jetbrains.exposed.sql.except
+import org.jetbrains.exposed.sql.intersect
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.koin.core.annotation.Single
@@ -12,11 +15,17 @@ import qna.domain.repository.UserAreasRepository
 @Single
 class UserAreasRepositoryImpl : UserAreasRepository {
     override fun getUsersByArea(questionArea: QuestionArea): List<Long> {
-        val muteUsers = MuteSettings.slice(MuteSettings.userId).selectAll()
+        val muteUsers = MuteSettings
+            .slice(MuteSettings.userId)
+            .selectAll()
+        val rightAwayUsers = NotificationPreferences
+            .slice(NotificationPreferences.userId)
+            .select { NotificationPreferences.preference eq NotificationPreference.RightAway }
         return UserAreas
             .slice(UserAreas.userId)
             .select { UserAreas.area eq questionArea }
             .except(muteUsers)
+            .intersect(rightAwayUsers)
             .map { it[UserAreas.userId].value }
     }
 }
