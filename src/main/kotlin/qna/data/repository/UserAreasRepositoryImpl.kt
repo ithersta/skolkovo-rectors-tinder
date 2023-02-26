465f9fc84b8790298dd6be1531e1ff2393572eea
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2023-02-26 21:49:42 +0300

    replace method.

--- src/main/kotlin/qna/data/repository/UserAreasRepositoryImpl.kt
+++ src/main/kotlin/qna/data/repository/UserAreasRepositoryImpl.kt
@@ -2,10 +2,11 @@ package qna.data.repository
 
 import auth.data.tables.UserAreas
 import mute.data.tables.MuteSettings
-import org.jetbrains.exposed.sql.except
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.koin.core.annotation.Single
+import qna.data.tables.QuestionAreas
+import qna.data.tables.Questions
 import qna.domain.entities.QuestionArea
 import qna.domain.repository.UserAreasRepository
 
@@ -18,5 +19,17 @@ class UserAreasRepositoryImpl : UserAreasRepository {
             .select { UserAreas.area eq questionArea }
             .except(muteUsers)
             .map { it[UserAreas.userId].value }
+    }
+
+    override fun getSubjectsByChatId(userId: Long, questionArea: Int): Map<Long, String> {
+        return UserAreas
+            .join(QuestionAreas, JoinType.INNER, additionalConstraint = { UserAreas.area eq QuestionAreas.area })
+            .join(Questions, JoinType.INNER, additionalConstraint = { Questions.id eq QuestionAreas.questionId })
+            .select(where = UserAreas.userId eq userId and Questions.isClosed.eq(false))
+            .filterNot { userId == it[Questions.authorId].value }
+            .filter { questionArea == it[QuestionAreas.area].ordinal }
+            .map { it[Questions.id].value to it[Questions.subject] }
+            .distinct()
+            .toMap()
     }
 }
