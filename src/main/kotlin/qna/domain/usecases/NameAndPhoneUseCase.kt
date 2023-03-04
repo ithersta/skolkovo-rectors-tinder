c82f3005bbfbba798b487ff21b3d539458f35a9d
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2023-03-04 17:55:21 +0300

    update

--- src/main/kotlin/qna/domain/usecases/NameAndPhoneUseCase.kt
+++ src/main/kotlin/qna/domain/usecases/NameAndPhoneUseCase.kt
@@ -2,14 +2,14 @@ package qna.domain.usecases
 
 import common.domain.Transaction
 import org.koin.core.annotation.Single
-import qna.domain.repository.ResponsesRepository
+import qna.domain.repository.ResponseRepository
 
 @Single
 class NameAndPhoneUseCase(
-    private val responsesRepository: ResponsesRepository,
+    private val responseRepository: ResponseRepository,
     private val transaction: Transaction
 ) {
     operator fun invoke(questionId: Long): Map<String, String> = transaction {
-        return@transaction responsesRepository.getRespondentIdByQuestion(questionId)
+        return@transaction responseRepository.getRespondentByQuestionId(questionId)
     }
 }
