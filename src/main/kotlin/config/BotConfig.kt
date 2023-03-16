2cf53e1373e22080299037276abae3ecb4eb206a
Ivan Molchanov <molchanov.ir@gmail.com>
2023-03-16 14:12:29 +0300

    some changes make curator id mandatory solve problem with GetUserRoleUseCaseTest

--- src/main/kotlin/config/BotConfig.kt
+++ src/main/kotlin/config/BotConfig.kt
@@ -4,12 +4,12 @@ private const val DEFAULT_BACKUP_USER_ID = 105293829L
 
 data class BotConfig(
     val adminId: Long?,
-    val curatorId: Long?,
+    val curatorId: Long,
     val backupUserId: Long
 )
 
 fun readBotConfig() = BotConfig(
     adminId = System.getenv()["ADMIN_ID"]?.toLong(),
-    curatorId = System.getenv()["CURATOR_ID"]?.toLong() ?: DEFAULT_BACKUP_USER_ID,
+    curatorId = System.getenv()["CURATOR_ID"]?.toLong()!!,
     backupUserId = System.getenv()["BACKUP_USER_ID"]?.toLong() ?: DEFAULT_BACKUP_USER_ID
 )
