9fb276afd0bce0d666e14c4b09bda2007c1867ff
IvanMolchanov <IvanMolchanov@users.noreply.github.com>
2023-03-16 13:32:03 +0000

    [MegaLinter] Apply linters fixes

--- src/test/kotlin/ModuleTest.kt
+++ src/test/kotlin/ModuleTest.kt
@@ -1,5 +1,4 @@
 import config.BotConfig
-import io.mockk.withInstanceFactory
 import org.junit.jupiter.api.Test
 import org.koin.dsl.koinApplication
 import org.koin.test.check.checkModules
@@ -7,12 +6,11 @@ import org.koin.test.check.checkModules
 internal class ModuleTest {
     @Test
     fun `Koin definitions are complete`() {
-        koinApplication{
+        koinApplication {
             modules(module)
             checkModules {
                 withInstance<BotConfig>(BotConfig(0L, 0L, 0L))
             }
         }
-
     }
 }
