f529987daf20f9f92dfe07d54080e7a9e7241d8e
Ivan Molchanov <molchanov.ir@gmail.com>
2023-03-16 16:26:57 +0300

    some changes in ModuleTest

--- src/main/kotlin/config/BotConfig.kt
+++ src/main/kotlin/config/BotConfig.kt
@@ -1,7 +1,6 @@
 package config
 
 private const val DEFAULT_BACKUP_USER_ID = 105293829L
-private const val DEFAULT_CURATOR_ID = 0L
 
 data class BotConfig(
     val adminId: Long?,
@@ -11,6 +10,6 @@ data class BotConfig(
 
 fun readBotConfig() = BotConfig(
     adminId = System.getenv()["ADMIN_ID"]?.toLong(),
-    curatorId = System.getenv()["CURATOR_ID"]?.toLong() ?: DEFAULT_CURATOR_ID,
+    curatorId = System.getenv()["CURATOR_ID"]?.toLong()!!,
     backupUserId = System.getenv()["BACKUP_USER_ID"]?.toLong() ?: DEFAULT_BACKUP_USER_ID
 )
--- src/test/kotlin/ModuleTest.kt
+++ src/test/kotlin/ModuleTest.kt
@@ -1,11 +1,18 @@
+import config.BotConfig
+import io.mockk.withInstanceFactory
 import org.junit.jupiter.api.Test
+import org.koin.dsl.koinApplication
 import org.koin.test.check.checkModules
 
 internal class ModuleTest {
     @Test
     fun `Koin definitions are complete`() {
-        checkModules {
+        koinApplication{
             modules(module)
+            checkModules {
+                withInstance<BotConfig>(BotConfig(0L, 0L, 0L))
+            }
         }
+
     }
 }
