9f9cbfeeb5505c41b3dfc57426c0f2cbf7180ca7
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-02 18:35:53 +0300

    Wrap long lines

--- src/main/kotlin/locations/domain/entities/Location.kt
+++ src/main/kotlin/locations/domain/entities/Location.kt
@@ -9,8 +9,26 @@ sealed interface Location {
         val parentId: Long
     }
 
-    data class Country(override val id: Long, override val name: String) : Location, WithChildren
-    data class District(override val id: Long, override val name: String, override val parentId: Long) : Location, WithParent, WithChildren
-    data class Region(override val id: Long, override val name: String, override val parentId: Long) : Location, WithParent, WithChildren
-    data class City(override val id: Long, override val name: String, override val parentId: Long) : Location, WithParent
+    data class Country(
+        override val id: Long,
+        override val name: String
+    ) : Location, WithChildren
+
+    data class District(
+        override val id: Long,
+        override val name: String,
+        override val parentId: Long
+    ) : Location, WithParent, WithChildren
+
+    data class Region(
+        override val id: Long,
+        override val name: String,
+        override val parentId: Long
+    ) : Location, WithParent, WithChildren
+
+    data class City(
+        override val id: Long,
+        override val name: String,
+        override val parentId: Long
+    ) : Location, WithParent
 }
