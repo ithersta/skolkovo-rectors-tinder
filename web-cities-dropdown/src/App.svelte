0d85dd5ddc36902356676aa5c628e8dea1f05d3c
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-02 23:54:32 +0300

    Use colors from telegram

--- web-cities-dropdown/src/App.svelte
+++ web-cities-dropdown/src/App.svelte
@@ -20,14 +20,18 @@
 
 <style>
     p {
-        color: var(--tg-theme-text-color)
+        color: var(--tg-theme-text-color);
+    }
+
+    .selected {
+        background-color: var(--tg-theme-button-color);
     }
 </style>
 
 <main>
     <Input bind:inputValue/>
     {#each filteredCities as city}
-        <p class="mx-2 px-2 py-3 w-auto text-slate-900 justify-start rounded-md {city === selectedCity ? 'bg-blue-200' : ''}"
+        <p class="mx-2 px-2 py-3 w-auto text-slate-900 justify-start rounded-md {city === selectedCity ? 'selected' : ''}"
            on:click={() => onCitySelected(city)}>{city}</p>
     {/each}
 </main>
