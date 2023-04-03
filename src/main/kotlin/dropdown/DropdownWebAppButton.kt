3e08fc065419acd8660d8d30c34dbab437f52ba2
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-03 15:28:12 +0300

    Move onDropdownWebAppResult to a separate file

--- src/main/kotlin/dropdown/DropdownWebAppButton.kt
+++ src/main/kotlin/dropdown/DropdownWebAppButton.kt
@@ -1,14 +1,7 @@
 package dropdown
 
-import auth.domain.entities.User
-import com.ithersta.tgbotapi.fsm.entities.triggers.Handler
-import com.ithersta.tgbotapi.fsm.entities.triggers.onWebAppData
-import common.telegram.DialogState
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.ReplyKeyboardRowBuilder
 import dev.inmo.tgbotapi.extensions.utils.types.buttons.webAppButton
-import dev.inmo.tgbotapi.types.message.ChatEvents.WebAppData
-import dev.inmo.tgbotapi.types.message.PrivateEventMessage
-import generated.StateFilterBuilder
 import io.ktor.http.*
 import io.ktor.server.util.*
 
@@ -28,13 +21,4 @@ fun ReplyKeyboardRowBuilder.dropdownWebAppButton(
         noneOption?.let { parameters.append("noneOption", it) }
         noneConfirmationMessage?.let { parameters.append("noneConfirm", it) }
     })
-}
-
-fun <S : DialogState, U : User> StateFilterBuilder<S, U>.onDropdownWebAppResult(
-    handler: Handler<DialogState, User, S, U, Pair<PrivateEventMessage<WebAppData>, String?>>
-) = onWebAppData { message ->
-    val result = message.chatEvent.data
-        .takeIf { it != "null" }
-        ?.removeSurrounding("\"")
-    handler(this, message to result)
 }
