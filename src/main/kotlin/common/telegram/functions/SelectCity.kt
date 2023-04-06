c3a32f78d6c50724ad603f257e70e801b58bc736
byzaya <byzaya777@gmail.com>
2023-04-06 17:10:34 +0300

    feat: display city dropdown

--- src/main/kotlin/common/telegram/functions/SelectCity.kt
+++ src/main/kotlin/common/telegram/functions/SelectCity.kt
@@ -5,20 +5,29 @@ import auth.telegram.Strings
 import auth.telegram.queries.*
 import com.ithersta.tgbotapi.fsm.builders.StateFilterBuilder
 import com.ithersta.tgbotapi.fsm.entities.triggers.onEnter
+import common.domain.Transaction
 import common.telegram.DialogState
 import dev.inmo.tgbotapi.extensions.api.answers.answer
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
+import dev.inmo.tgbotapi.extensions.utils.types.buttons.flatReplyKeyboard
 import dev.inmo.tgbotapi.types.UserId
+import dropdown.dropdownWebAppButton
 import generated.onDataCallbackQuery
+import org.koin.core.component.inject
+import organizations.domain.repository.CityRepository
 
 fun <State : DialogState> StateFilterBuilder<DialogState, User, State, *, UserId>.selectCity(
     onFinish: (State, String) -> DialogState
 ) {
+    val cityRepository: CityRepository by inject()
+    val transaction: Transaction by inject()
     onEnter {
         sendTextMessage(
             it,
-            Strings.AccountInfo.ChooseCountry,
-            replyMarkup = TODO()
+            Strings.AccountInfo.ChooseCity,
+            replyMarkup = flatReplyKeyboard {
+                dropdownWebAppButton("dsf", options = transaction { cityRepository.getAll() }.map { it.name }, noneConfirmationMessage = "ТОЧНО??", noneOption = "МОЕГО ГОРОДА НЕТ В СПИСКЕ")
+            }
         )
     }
     onDataCallbackQuery(SelectCountryQuery::class) { (data, query) ->
