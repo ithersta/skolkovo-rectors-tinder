16e51d1722a09dab4f1d4c3ec5013b75f25af272
byzaya <byzaya777@gmail.com>
2023-04-11 17:04:09 +0300

    style: add comments

--- src/main/kotlin/event/telegram/Strings.kt
+++ src/main/kotlin/event/telegram/Strings.kt
@@ -10,34 +10,32 @@ object Strings {
         const val InputName = "Введите название мероприятия"
         const val InputBeginDateTime = "Введите дату и время начала мероприятия в формате дд.ММ.гггг чч:мм"
         const val InputEndDateTime = "Введите дату и время окончания мероприятия в формате дд.ММ.гггг чч:мм"
-        const val InputDescription = "Введите краткое описание мероприятия. " +
-                "\nЕсли такого не имеется, нажмите соответствующую кнопку"
+        const val InputDescription =
+            "Введите краткое описание мероприятия. " + "\nЕсли такого не имеется, нажмите соответствующую кнопку"
         const val NoDescription = "Нет описания"
         const val InputUrl = "Введите ссылку на мероприятие"
 
         // TODO тут придумать сообщение
-        const val EventIsCreated = "Мероприятие добавлено в календарь! " +
-                "Участникам отправлено оповещение о новом мероприятии"
+        const val EventIsCreated =
+            "Мероприятие добавлено в календарь! " + "Участникам отправлено оповещение о новом мероприятии"
 
         const val EventNotCreated = "Мероприятие не создано"
         const val InvalidDataFormat = "Введён неверный формат данных. "
-        fun message(event: Event) =
-            buildEntities {
-                bold("Название: ")
-                regular(event.name)
-                boldln("Дата и время начала: ")
-                regular(dateTimeFormatter.format(event.timestampBegin.toLocalDateTime()))
-                boldln("Дата и время окончания: ")
-                regular(dateTimeFormatter.format(event.timestampEnd.toLocalDateTime()))
-                if (event.description.isNotEmpty()) {
-                    boldln("Краткое описание: ")
-                    regular(event.description)
-                }
-                boldln("Ссылка: ")
-                regular(event.url)
-                regularln("")
-                italicln("Все верно?")
+        fun message(event: Event) = buildEntities {
+            bold("Название: ")
+            regular(event.name)
+            boldln("Дата и время начала: ")
+            regular(dateTimeFormatter.format(event.timestampBegin.toLocalDateTime()))
+            boldln("Дата и время окончания: ")
+            regular(dateTimeFormatter.format(event.timestampEnd.toLocalDateTime()))
+            if (event.description.isNotEmpty()) {
+                boldln("Краткое описание: ")
+                regular(event.description)
             }
+            boldln("Ссылка: ")
+            regular(event.url)
+            italicln("\nВсе верно?")
+        }
     }
 
     object RemoveEvent {
@@ -47,24 +45,22 @@ object Strings {
         const val NotRemove = "Мероприятие не удалено ❌"
     }
 
-    private val dateTimeFormatter = DateTimeFormatter
-        .ofLocalizedDateTime(FormatStyle.LONG)
+    private val dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG)
 
     // TODO тут придумать сообщение для рассылки пользователям
 
-    const val New = "📅Новое мероприятие"
-    fun eventMessage(event: Event) =
-        buildEntities {
-            bold(event.name)
-            regularln("🕓 ")
-            regular(
-                dateTimeFormatter.format(event.timestampBegin.toLocalDateTime()) + " - "
-                        + dateTimeFormatter.format(event.timestampEnd.toLocalDateTime())
-            )
-            if (event.description.isNotEmpty()) {
-                regularln(event.description)
-            }
-            regularln("🔗")
-            link("Подробнее", event.url)
+    const val New = "📅Новое мероприятие\n"
+    fun eventMessage(event: Event) = buildEntities {
+        bold(event.name)
+        regularln("🕓 ")
+        regular(
+            dateTimeFormatter.format(event.timestampBegin.toLocalDateTime()) +
+                    " - " + dateTimeFormatter.format(event.timestampEnd.toLocalDateTime())
+        )
+        if (event.description.isNotEmpty()) {
+            regularln(event.description)
         }
+        regularln("🔗")
+        link("Подробнее", event.url)
+    }
 }
--- src/main/kotlin/event/telegram/flows/RemoveEventFlow.kt
+++ src/main/kotlin/event/telegram/flows/RemoveEventFlow.kt
@@ -7,17 +7,24 @@ import com.ithersta.tgbotapi.fsm.entities.triggers.onText
 import common.telegram.DialogState
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
 import dev.inmo.tgbotapi.types.UserId
+import event.telegram.Strings
 import menus.states.MenuState
 
 fun StateMachineBuilder<DialogState, User, UserId>.removeEventFlow() {
     role<User.Normal> {
         state<MenuState.RemoveEventState> {
             onEnter {
-                sendTextMessage(it, "TODO")
+                sendTextMessage(it, Strings.RemoveEvent.ChooseEvent)
+                //TODO пагинация
             }
             onText {
                 state.override { DialogState.Empty }
             }
+            //сначала будет список inline кнопок с названиями мероприятий(пагинация)
+            //далее при нажатии на кнопку, выводится информация о мероприятии +
+            // вы действительно хотите удалить мероприятие?
+            //и 2 кнопки - да/нет
+            //все это должно быть в anyState
         }
     }
 }
