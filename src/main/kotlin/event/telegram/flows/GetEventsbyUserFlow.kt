0e3b66181ed75721fe7a36db840fb46c94da2d0f
byzaya <byzaya777@gmail.com>
2023-04-13 00:24:23 +0300

    fix: improve output event calendar

--- src/main/kotlin/event/telegram/flows/GetEventsbyUserFlow.kt
+++ src/main/kotlin/event/telegram/flows/GetEventsbyUserFlow.kt
@@ -6,7 +6,6 @@ import com.ithersta.tgbotapi.fsm.entities.triggers.onEnter
 import common.telegram.DialogState
 import dev.inmo.tgbotapi.extensions.api.send.sendTextMessage
 import dev.inmo.tgbotapi.types.UserId
-import dev.inmo.tgbotapi.utils.buildEntities
 import event.domain.usecases.GetEventsUseCase
 import event.telegram.Strings
 import menus.states.MenuState
@@ -16,13 +15,8 @@ fun StateMachineBuilder<DialogState, User, UserId>.getEventsByUserFlow() {
     val getEventsUseCase: GetEventsUseCase by inject()
     role<User.Normal> {
         state<MenuState.Events> {
-            //TODO переделать, так как выводится не то
             onEnter {
-                val entities = buildEntities {
-                    getEventsUseCase.invoke().forEach() { event ->
-                        addAll(Strings.eventMessage(event))
-                    }
-                }
+                val entities = getEventsUseCase().flatMap { event -> Strings.eventMessage(event) }
                 sendTextMessage(it, entities)
                 state.override { DialogState.Empty }
             }
