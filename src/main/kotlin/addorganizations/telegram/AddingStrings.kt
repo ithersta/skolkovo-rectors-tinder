9ccdbed2bb6a68fc846f4f862f94a9bf7850f536
Ivan Molchanov <molchanov.ir@gmail.com>
2023-04-13 17:27:45 +0300

    change strings

--- src/main/kotlin/addorganizations/telegram/AddingStrings.kt
+++ src/main/kotlin/addorganizations/telegram/AddingStrings.kt
@@ -15,55 +15,52 @@ object AddingStrings {
         confirmation = "Пожалуйста, убедитесь, что этого города действительно нет в списке.\n\n" +
             "Если вы подтвердите отсутствие города, вам будет предложено ввести его.",
         noCity = DropdownWebAppStrings.CityDropdown.noCity,
-        chooseCity = DropdownWebAppStrings.CityDropdown.chooseCity
+        chooseCity = "Проверьте отстутствие города в списке"
     )
     val CityOrganizationDropdown = StringsCity(
         button = DropdownWebAppStrings.CityDropdown.button,
         confirmation = "Пожалуйста, убедитесь, что этого города действительно нет в списке.\n\n" +
             "Если вы подтвердите отсутствие города, вы не сможете продолжить добавление организации.",
         noCity = DropdownWebAppStrings.CityDropdown.noCity,
-        chooseCity = DropdownWebAppStrings.CityDropdown.chooseCity
+        chooseCity = "Выберите город, в котором расположена организация"
     )
     val OrganizationDropdown = StringsOrganization(
         button = DropdownWebAppStrings.OrganizationDropdown.button,
         confirmation = "Пожалуйста, убедитесь, что этой организации действительно нет в списке.\n\n" +
             "Если вы подтвердите отсутствие организации, вам будет предложено ввести её.",
         noOrganization = DropdownWebAppStrings.OrganizationDropdown.noOrganization,
-        chooseOrganization = DropdownWebAppStrings.OrganizationDropdown.chooseOrganization
+        chooseOrganization = "Просмотрите список организаций и проверьте её отсутствие"
     )
-    fun havingOrganizationAdmin(city: String, organization: String) = "Вы привязали организацию: $organization\n" +
+    private const val tipStart = "Если вы прервали регистрацию, введите /start."
+    fun havingOrganizationAdmin(city: String, organization: String) = "Вы привязали организацию: $organization\n\n" +
         "к городу: $city."
-    fun havingOrganizationUser(city: String, organization: String) = "Вашу организацию: $organization\n" +
-        "привязали к городу: $city.\n" +
-        "Чтобы зарегистрироваться введите /start."
+    fun havingOrganizationUser(city: String, organization: String) = "Вашу организацию: $organization\n\n" +
+        "привязали к городу: $city.\n\n$tipStart"
     fun havingCityAdmin(city: String) = "Вы выбрали существующий город: $city"
-    fun havingCityUser(city: String) = "Ваш город нашли в таблице под названием: $city.\n" +
-        "Чтобы зарегистрироваться введите /start."
+    fun havingCityUser(city: String) = "Ваш город нашли в таблице под названием: $city.\n\n$tipStart"
     fun addCityAdmin(city: String) = "Вы добавили город: $city."
-    fun addCityUser(city: String) = "Ваш город добавили: $city.\n" +
-        "Этот город теперь можно выбрать в своем профиле." +
-        "Чтобы зарегистрироваться введите /start."
-    fun addOrganizationAdmin(city: String, organization: String) = "Вы создали организацию: $organization\n" +
+    fun addCityUser(city: String) = "Ваш город добавили: $city.\n\n" +
+        "Этот город теперь можно выбрать в своем профиле.$tipStart"
+    fun addOrganizationAdmin(city: String, organization: String) = "Вы создали организацию: $organization\n\n" +
         "и привязали её к городу: $city."
-    fun addOrganizationUser(city: String, organization: String) = "Вашу организацию добавили: $organization\n" +
-        "и привязали её к городу: $city.\n" +
-        "Чтобы зарегистрироваться введите /start."
+    fun addOrganizationUser(city: String, organization: String) = "Вашу организацию добавили: $organization\n\n" +
+        "и привязали её к городу: $city.\n\n$tipStart"
     fun confirmationAddingCityAdmin(city: String) =
-        "Предложено добавить новый город: $city\n"
-    fun sentCity(city: String) = "Вы отправили запрос на добавление города: $city.\n" +
-        "Пожалуйста, подождите, пока администраторы добавят его, когда это произойдёт вам прийдет уведомление и вы" +
+        "Предложено добавить новый город: $city"
+    fun sentCity(city: String) = "Вы отправили запрос на добавление города: $city.\n\n" +
+        "Пожалуйста, подождите, пока администраторы добавят его, когда это произойдёт, вам прийдет уведомление и вы" +
         " сможете зарегистрироваться"
     fun sentOrganization(organization: String, city: String) = "Вы отправили запрос на добавление организации:" +
-        " $organization.\nВ городе: $city\n" +
-        "Пожалуйста, подождите, пока администраторы добавят её, когда это произойдёт вам прийдет уведомление и вы" +
+        " $organization.\n\nВ городе: $city\n\n" +
+        "Пожалуйста, подождите, пока администраторы добавят её, когда это произойдёт, вам прийдет уведомление и вы" +
         " сможете зарегистрироваться"
 
     fun confirmationAddingOrganizationAdmin(having: Boolean, city: String, organization: String) =
         "${
             if (having) {
-                "Предложено добавить в старый город: $city"
+                "Предложено добавить в старый город: $city\n"
             } else {
-                "Предложено добавить в новый город: $city\n" +
+                "Предложено добавить в новый город: $city\n\n" +
                     "Это значит, что выше должно быть предложение добавить этот город."
             }
         }\nОрганизацию: $organization"
