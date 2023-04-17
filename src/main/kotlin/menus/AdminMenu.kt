2fe057e130a9da3f668d52717fb8e9760e532284
byzaya <byzaya777@gmail.com>
2023-04-17 21:34:41 +0300

    fix: fix style

--- src/main/kotlin/menus/AdminMenu.kt
+++ src/main/kotlin/menus/AdminMenu.kt
@@ -6,10 +6,10 @@ import auth.domain.entities.User
 import auth.telegram.Strings
 import common.telegram.DialogState
 import common.telegram.strings.CommonStrings
+import event.telegram.flows.sendListOfEvents
 import generated.menu
 import menus.states.MenuState
 import menus.strings.MenuStrings
-import event.telegram.flows.sendListOfEvents
 
 val adminMenu = menu<User.Admin>(Strings.RoleMenu.Admin, DialogState.Empty) {
     menu()
