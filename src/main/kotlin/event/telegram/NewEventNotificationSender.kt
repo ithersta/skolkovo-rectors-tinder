c2ca3e679e7745c8a217e5cc9e63c61abf270e27
Владислав Шиляев <schilyaev.vlad@gmail.com>
2023-04-23 21:57:40 +0300

    Add exception logging in NewEventNotificationSender

--- src/main/kotlin/event/telegram/NewEventNotificationSender.kt
+++ src/main/kotlin/event/telegram/NewEventNotificationSender.kt
@@ -1,12 +1,12 @@
 package event.telegram
 
 import common.telegram.MassSendLimiter
-import dev.inmo.micro_utils.coroutines.launchSafelyWithoutExceptions
 import dev.inmo.tgbotapi.extensions.behaviour_builder.BehaviourContext
 import dev.inmo.tgbotapi.types.toChatId
 import event.telegram.flows.GetNewEventNotificationFlowUseCase
 import event.telegram.flows.sendEventMessage
 import io.github.oshai.KotlinLogging
+import kotlinx.coroutines.launch
 import kotlinx.datetime.TimeZone
 import org.koin.core.annotation.Single
 
@@ -19,14 +19,18 @@ class NewEventNotificationSender(
     private val timeZone: TimeZone
 ) {
     context(BehaviourContext)
-    fun setup() = launchSafelyWithoutExceptions {
-        getNewEventNotificationFlowUseCase().collect { notification ->
-            massSendLimiter.wait()
-            runCatching {
-                sendEventMessage(notification.userId.toChatId(), notification.event, timeZone)
-            }.onFailure { exception ->
-                logger.error("Couldn't send new event notification to ${notification.userId}", exception)
+    fun setup() = launch {
+        runCatching {
+            getNewEventNotificationFlowUseCase().collect { notification ->
+                massSendLimiter.wait()
+                runCatching {
+                    sendEventMessage(notification.userId.toChatId(), notification.event, timeZone)
+                }.onFailure { exception ->
+                    logger.error("Couldn't send new event notification to ${notification.userId}", exception)
+                }
             }
+        }.onFailure { exception ->
+            logger.error("Exception during collection", exception)
         }
     }
 }
